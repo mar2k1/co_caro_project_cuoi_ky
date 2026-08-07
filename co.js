d0f7ae3f62270dd678e50e972db39fa8262d1b28
mar2k1 <trankhanhquynh172@gmail.com>
2026-08-07 20:35:23 +0700

    khai báo mảng, biến và hàm bắt đầu trò chơi

--- co.js
+++ co.js
@@ -8,3 +8,8 @@ const winConditions = [
     [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6]
 ];
+let options = ["","","","","","","","","",];
+let currentPlayer = "O";
+let running = false;
+
+initGame();
