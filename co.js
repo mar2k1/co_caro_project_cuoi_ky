ca0c8722c052f58cdd27e7da5f55fd70d6aef1df
mar2k1 <trankhanhquynh172@gmail.com>
2026-08-07 20:32:29 +0700

    điều kiện thắng

--- co.js
+++ co.js
@@ -1,2 +1,10 @@
 const cells = document.querySelectorAll('.cell');
-const statusText = document.querySelector('#status')
+const statusText = document.querySelector('#status');
+const turnText = document.querySelector('#turn');
+const restart = document.querySelector('#restart');
+
+const winConditions = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+];
