79558684c9c48d1186ce78df98e5fa4bb3cb1b8e
mar2k1 <trankhanhquynh172@gmail.com>
2026-08-09 22:38:29 +0700

    sửa hàm chekc người chiến thắng

--- co.js
+++ co.js
@@ -90,21 +90,19 @@ function changePlayer() {
 
 function checkWinner() {
     let roundWon = false;
-    for (let i = 0; i < winConditions.length; i++) {
-        const condition = winConditions[i];
+    for (let condition of winConditions) {
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
         const cellC = options[condition[2]];
-    if (cellA === "" || cellB === "" || cellC === "") {
-        continue;
-    }
+    if (cellA === "" || cellB === "" || cellC === "") continue;
     if (cellA === cellB && cellB === cellC) {
         roundWon = true;
         break;
     }
 }
     if (roundWon) {
-        statusText.textContent = `Người chơi ${currentPlayer} chiến thắng!`;
+        const winnerName = (gameMode === 'pve' && currentPlayer === 'O') ? "Máy" : `Người chơi ${currentPlayer}`;
+        statusText.textContent = `${winnerName} chiến thắng!`;
         running = false;
     } else if (!options.includes("")) {
         statusText.textContent = "Hòa nhau!";
